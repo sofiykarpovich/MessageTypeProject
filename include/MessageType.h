343a1d99dbff3789c9348e293d0e3c5d26910ece
sofiykarpovich <sofiykarpovich@icloud.com>
2025-10-30 08:52:03 +0300

    Update MessageType.h

--- include/MessageType.h
+++ include/MessageType.h
@@ -1,13 +1,10 @@
 #pragma once
 
-
 #include <string>
 #include <string_view>
 #include <iosfwd>
 
-
 namespace messaging {
-
 
 enum class MessageType {
 System,
@@ -15,10 +12,8 @@ Server,
 Session
 };
 
-
 [[nodiscard]] std::string ToString(MessageType type) noexcept;
 [[nodiscard]] bool FromString(std::string_view text, MessageType& out) noexcept;
-
 
 [[nodiscard]] inline std::string Serialize(MessageType type) noexcept { return ToString(type); }
 [[nodiscard]] inline bool Deserialize(std::string_view text, MessageType& out) noexcept { return FromString(text, out); }
@@ -26,6 +21,5 @@ Session
 
 std::ostream& operator<<(std::ostream& os, MessageType type);
 std::istream& operator>>(std::istream& is, MessageType& type);
-
 
 } // namespace messaging
