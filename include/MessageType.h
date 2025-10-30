ff3091c04ba8d994a393c91d592dc499330c70d7
sofiykarpovich <sofiykarpovich@icloud.com>
2025-10-30 08:51:17 +0300

    Update MessageType.h

--- include/MessageType.h
+++ include/MessageType.h
@@ -16,11 +16,11 @@ Session
 };
 
 
-[[nodiscard]] std::string ToString(MessageType type);
+[[nodiscard]] std::string ToString(MessageType type) noexcept;
 [[nodiscard]] bool FromString(std::string_view text, MessageType& out) noexcept;
 
 
-[[nodiscard]] inline std::string Serialize(MessageType type) { return ToString(type); }
+[[nodiscard]] inline std::string Serialize(MessageType type) noexcept { return ToString(type); }
 [[nodiscard]] inline bool Deserialize(std::string_view text, MessageType& out) noexcept { return FromString(text, out); }
 
 
